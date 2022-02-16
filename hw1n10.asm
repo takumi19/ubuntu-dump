abf8d5d4f4865d5f01d8eb3435cb87759c6363d9
takumi19 <takumi@LAPTOP-KT26CSP4.localdomain>
2022-02-16 19:07:24 +0300

    hw1n10

---
+++ hw1n10.asm
@@ -0,0 +1,19 @@
+%include "io.inc"
+
+section .text
+global CMAIN
+CMAIN:
+  GET_DEC 4, ebx
+  GET_DEC 4, ecx
+  mov eax, 83
+  mul ebx
+  add ebx, ecx
+  and ebx, 1
+  cmp ebx, 1
+  je label
+  xor eax, eax
+  ret
+label:
+  sub eax, 41
+  xor eax, eax
+  ret
